fee17d73560436ffd78cb5dd569d73597ebf42f0
Lex Wuestenenk <s1197063@student.windesheim.nl>
2025-05-21 19:53:38 +0200

    Ubuntu template

---
+++ ubuntu/variables.tf
@@ -0,0 +1,14 @@
+variable "vsphere_user" {}
+variable "vsphere_password" {}
+variable "vsphere_server" {}
+
+variable "datacenter" {}
+variable "datastore" {}
+variable "cluster" {}
+variable "network" {}
+
+variable "template_name" {}
+variable "vm_name" {}
+
+variable "ipv4_address" {}
+variable "ipv4_gateway" {}
